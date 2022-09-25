16ffeb14b6e0e83df5caba946bd74b315dee0c1c
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-09-25 13:03:56 +0530

    manage local state for the todo text and todos list

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { Dispatch } from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({
+    todo,
+    setTodo,
+    handleAdd
+}) => {
     return (
-        <form className="input">
-            <input type="input" placeholder="Enter a text" className="input__box" />
+        <form className="input" onSubmit={handleAdd}>
+            <input 
+                type="input" 
+                placeholder="Enter a text" 
+                className="input__box" 
+                value={todo} 
+                onChange={(e) => setTodo(e.target.value)} 
+            />
             <button className="input_submit" type="submit">Go</button>
         </form>
     )
